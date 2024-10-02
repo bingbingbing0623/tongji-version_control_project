cffa813ab2db9e11c65f177d164fc8c10012012e
GitHub Action <action@github.com>
2024-10-02 13:12:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/bingbingbing0623/tongjiversioncontrolproject/MyBundle.kt
+++ src/main/kotlin/com/github/bingbingbing0623/tongjiversioncontrolproject/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.bingbingbing0623.tongjiversioncontrolproject
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/bingbingbing0623/tongjiversioncontrolproject/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/bingbingbing0623/tongjiversioncontrolproject/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.bingbingbing0623.tongjiversioncontrolproject.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
